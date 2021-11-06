c2af99c7a3a23accd175a70b25f8a395f9871fd0
Paul Nonso Ihejirika <89712081+PaulNonz@users.noreply.github.com>
2021-11-06 23:04:26 +0100

    commit

---
+++ js/style.js
@@ -0,0 +1,12 @@
+// .hero-bg{
+//     display: flex;
+//     width: auto;
+// }
+
+// /* create our pseudo element */
+// .hero-bg::before {
+//     content: "";
+//     background-image: url('./img/bck.JPG');
+//     background-repeat: no-repeat;
+//     background-size: cover;
+//     }
